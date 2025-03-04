683d96dc975e4d6384eb9a0bd1e0978d000e9e8c
guirista <billyblaze@rocketmail.com>
2025-03-04 14:29:32 +0100

    Add files via upload

--- streamlit_app3.py
+++ streamlit_app3.py
@@ -3,8 +3,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pickle
-import joblib
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
 
 df = pd.read_csv('train.csv')
 
@@ -13,8 +18,6 @@ st.sidebar.title("Table of contents")
 pages=["Exploration", "DataVizualization", "Modelling"]
 page=st.sidebar.radio("Go to", pages)
 
-joblib.load('model')
-loaded_model = pickle.load(open('model','rb'))
 
 if page == pages[0] :
     st.write('### Presentation of data')
@@ -60,63 +63,4 @@ if page == pages[1] :
     sns.heatmap(df.select_dtypes(['number']).corr(), ax=ax)
     st.write(fig)
 
-if page == pages[2] :
     st.write('### Modelling')
-
-    df = df.drop(['PassengerId','Name','Ticket','Cabin'], axis=1)
-
-    y= df['Survived']
-    X_cat  =df[['Pclass','Sex','Embarked']]
-    X_num = df[['Age','Fare','SibSp','Parch']]
-
-    for col in X_cat.columns:
-        X_cat[col] = X_cat[col].fillna(X_cat[col].mode()[0])
-    for col in X_num.columns:
-        X_num[col] = X_num[col].fillna(X_num[col].median())
-    X_cat_scaled = pd.get_dummies(X_cat, columns=X_cat.columns)
-    X = pd.concat([X_cat_scaled, X_num], axis=1)
-
-    from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=123)
-
-    from sklearn.preprocessing import StandardScaler
-    scaler = StandardScaler()
-    X_train[X_num.columns] = scaler.fit_transform(X_train[X_num.columns])
-    X_test[X_num.columns]  = scaler.transform(X_test[X_num.columns])
-
-    from sklearn.ensemble import RandomForestClassifier
-    from sklearn.svm import SVC
-    from sklearn.linear_model import LogisticRegression
-    from sklearn.metrics import confusion_matrix
-
-    def prediction(classifier):
-        if classifier == 'Random Forest':
-            clf = RandomForestClassifier()
-        elif classifier == 'SVC':
-            clf = SVC()
-        elif classifier == 'Logistic Regression':
-            clf = LogisticRegression()
-        clf.fit(X_train,y_train)
-        return clf
-
-    def scores(clf,choice):
-        if choice == 'Accuracy':
-            return clf.score(X_test,y_test)
-        elif choice == 'Confusion matrix':
-            return confusion_matrix(y_test, clf.predict(X_test))
-
-    choice =  ['Random Forest', 'SVC', 'Logistic Regression']
-    option = st.selectbox('Choice of the model', choice)
-    st.write('The chosen model is:', option)
-
-    clf = prediction(option)
-    display = st.radio('What do you want to show?', ('Accuracy','Confusion matrix'))
-    if display == 'Accuracy':
-        st.write(scores(clf,display))
-    elif display == 'Confusion matrix':
-        st.dataframe(scores(clf,display))
-    import joblib
-    joblib.dump(clf, 'model')
-    import pickle
-    pickle.dump(clf, open('model','wb'))
-
