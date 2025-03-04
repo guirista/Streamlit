6a8ac472c1c5d2ccc1c9637fca660e5881d54493
guirista <billyblaze@rocketmail.com>
2025-03-04 14:31:28 +0100

    Add files via upload

--- streamlit_app3.py
+++ streamlit_app3.py
@@ -1,14 +1,7 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
 
 
 df = pd.read_csv('train.csv')
