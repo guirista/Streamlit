17237aab27205be23b0b310623ba6f584ed100a9
guirista <billyblaze@rocketmail.com>
2025-03-05 13:20:00 +0100

    Update streamlit_app3.py

--- streamlit_app3.py
+++ streamlit_app3.py
@@ -72,5 +72,4 @@ if page == pages[2] :
     X_cat_scaled = pd.get_dummies(X_cat, columns=X_cat.columns)
     X = pd.concat([X_cat_scaled, X_num], axis=1)
     
-    from sklearn.model_selection import train_test_split
     X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=123)
