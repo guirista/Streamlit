b5b55d8575a4ba60a323928a613e3a3ede0f28e2
guirista <billyblaze@rocketmail.com>
2025-10-05 17:24:08 +0200

    Update page4.py

--- page4.py
+++ page4.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from scikit-learn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 
-from scikit-learn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression
 
 
 st.title("Machine Learning")
