0630ab17a00335a221b8f7db985709546736ae56
guirista <billyblaze@rocketmail.com>
2025-10-05 15:37:10 +0200

    Update page4.py

--- page4.py
+++ page4.py
@@ -2,10 +2,7 @@ import streamlit as st
 import numpy as np
 import streamlit_data
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.linear_model import LinearRegression
+
 
 st.title("Machine Learning")
 
