ebea6f029fb7296de088b5f96e97e52fc61772ed
guirista <billyblaze@rocketmail.com>
2025-10-06 13:40:24 +0200

    Update page2.py

--- page2.py
+++ page2.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import datetime
+from bokeh.plotting import figure, show, output_notebook
 
 import plotly.graph_objects as go 
 from plotly.subplots import make_subplots
